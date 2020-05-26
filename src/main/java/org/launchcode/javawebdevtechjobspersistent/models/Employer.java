4408cf0d90ddebbf068707b066323629dcff27fb
pheave200 <phil_heavey@cable.comcast.com>
2020-05-26 11:28:31 -0400

    parts 1 & 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Lacation cannot be empty")
+    private String location;
 
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Skill cannot be blank.")
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
