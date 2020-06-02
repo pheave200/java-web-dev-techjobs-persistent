448066855e122c1a0b20f91fb7ff6ed035b060e2
pheave200 <phil_heavey@cable.comcast.com>
2020-06-02 10:19:35 -0400

    final?(part2)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
@@ -1,6 +1,7 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
 import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
 import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,9 @@ public class ListController {
     private JobRepository jobRepository;
 
     @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
     SkillRepository skillRepository;
 
     static HashMap<String, String> columnChoices = new HashMap<>();
@@ -38,6 +42,9 @@ public class ListController {
 
     @RequestMapping("")
     public String list(Model model) {
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("all", jobRepository.findAll());
+        model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute("skills", skillRepository.findAll());
         return "list";
     }
