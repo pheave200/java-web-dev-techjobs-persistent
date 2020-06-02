aa1efaf3b115f75e48f006ab0e97e0a89f12e901
pheave200 <phil_heavey@cable.comcast.com>
2020-06-02 10:09:41 -0400

    final?

--- queries.sql
+++ queries.sql
@@ -1,7 +1,15 @@
 ## Part 1: Test it with SQL
+SELECT * FROM techjobs.job;
 
 ## Part 2: Test it with SQL
+SELECT * FROM techjobs.employer
+WHERE location = "St Louis";
 
 ## Part 3: Test it with SQL
+DROP TABLE techjobs.job;
 
 ## Part 4: Test it with SQL
+SELECT name, description
+FROM techjobs.skill
+INNER JOIN techjobs.job ON techjobs.skills_id = techjobs.job_skills.jobs_id
+WHERE jobs_id != null;
